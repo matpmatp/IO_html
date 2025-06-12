850c39022ca38d452f065343408bf1a34ca4e12f
FilipChwaleba <152343853+FilipChwaleba@users.noreply.github.com>
2025-06-12 11:41:29 +0200

    Update ioSchemat.cpp

--- ioSchemat.cpp
+++ ioSchemat.cpp
@@ -14,7 +14,7 @@ public:
     std::string nazwisko;
 
     //metody
-    bool zaloguj(std::string login, std::string has≥o);
+    bool zaloguj(std::string login, std::string has≈Ço);
     bool wyloguj();
 };
 
@@ -25,7 +25,7 @@ public:
     int id_studenta;
     std::string uczelnia;
     int nr_pokoju;
-    enum wyposazenie{klucz, koldra, poduszka, poszwa, poszewka, przeúcierad≥o, kabel_internetowy, pilot, koc};
+    enum wyposazenie{klucz, koldra, poduszka, poszwa, poszewka, prze≈õcierad≈Ço, kabel_internetowy, pilot, koc};
 
     //metody
      void zglosUsterke(std::string opis, char* filepath);
@@ -48,26 +48,59 @@ public:
 };
 
 
-class Pracownik:public User { //po tym dziedziczy Admin, Zarzπdca DB, Us≥ugi i Recepcja
+class Pracownik:public User { //po tym dziedziczy Admin, ZarzƒÖdca DB, Us≈Çugi i Recepcja
     int id_pracownika;
 
+    //metody
     bool generujRaport(int rodzaj);
 };
 class Recepcja:public Pracownik {
+public:
+    //metody
+    bool odwiedziny(Pokoj pokoj, Student student);
+    void wydajWyposa≈ºenie(Student student);
+    void zdawaniePokoju(Pokoj pokoj, Student student);
+};
+class admin:public Pracownik{
+public:
+    //metody
+    bool przetworzWniosek();//stworzyƒá klase wniosek
+    bool zarejestrujStudenta(User toBeStudent);
+    void zdawaniePokoju(Pokoj pokoj, Student student);
+};
+class uslugi:public Pracownik{
+public:
+    void kontrolaPokoju(Pokoj pokoj);
 
 };
-class dbAdmin:public Pracownik{};
-class uslugi:public Pracownik{};
-class konserwator:public uslugi{};
-class sprzatanie:public uslugi{};
+class konserwator:public uslugi{
+public:
+   void przyjmijUsterke(Usterka usterka);
+   bool zmienStatusUsterki(Usterka usterka, int nowyStatus); //wywo≈Çanie wywo≈Ça metode w usterce co zmieni jej status na przekazany 
+   bool zdawaniePokojuKonserwacha(Pokoj pokoj);
+         
+};
+class sprzatanie :public uslugi {
+public:
+    void sprzataj();//iks de de mots zjadacz czasu
+    bool zdawaniePokojuSprzatanie(Pokoj pokoj);
+
+};
 
 
 
 
 
 
-class Op≥ata {
+class Op≈Çata {
+    int id_oplaty;
+    int wartosc;
+    int id_studenta;
+    std::string opis;
+    std::string dataWykonania;
 
+    //metody
+    bool wykonajOplate();
 };
 class Usterka {
     int id_uslugi;
@@ -91,11 +124,23 @@ class Raport {
     enum okers{miesiac, semestr, pol_roku, rok};
     
     //metody
-    bool stworzRaport(); //wywo≥ywane przez pracownik::generujRaport() potem przez przekazany argument wywo≥a metode dla wybranego typu raportu(jeden z tych poniøej)
+    bool stworzRaport(); //wywo≈Çywane przez pracownik::generujRaport() potem przez przekazany argument wywo≈Ça metode dla wybranego typu raportu(jeden z tych poni≈ºej)
 };
 class raportKonserwacyjny :public Raport {
 public:
+    std::string opis;
 };
-class raportFinansowy: public Raport{};
-class raportSzkod: public Raport{};
-class raportZamieszkania: public Raport{};
+class raportFinansowy : public Raport {
+public:
+    int wydatki;
+    int nalezlnosci;
+    int wplaty;
+};
+class raportSzkod : public Raport {
+public:
+    std::vector<Usterka> usterki;
+};
+class raportZamieszkania: public Raport{
+public:
+    std::vector<Pokoj>statusZamieszkania;
+};
